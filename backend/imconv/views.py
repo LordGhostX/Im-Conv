583798c448a3b6a51faf643e50af217e726add55
LordGhostX <solomonghost9@gmail.com>
2019-09-18 04:10:28 +0000

    integrated frontend

--- backend/imconv/views.py
+++ backend/imconv/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
+    data = {}
+    return render(request, "index.html", data)
