2525404ec7f5c29fc16cb22fe1673a5cd26620af
LordGhostX <solomonghost9@gmail.com>
2019-09-18 04:32:06 +0000

    converted base64 to image

---
+++ backend/imconv/conv.py
@@ -0,0 +1,18 @@
+from random import randint
+import base64
+import re
+
+def decode_base64(data, altchars=b'+/'):
+    data = re.sub(rb'[^a-zA-Z0-9%s]+' % altchars, b'', data)
+    missing_padding = len(data) % 4
+    if missing_padding:
+        data += b'='* (4 - missing_padding)
+    return base64.b64decode(data, altchars)
+
+def converter(img_token, extension="PNG", rand_min=1, rand_max=10000):
+    imgdata = decode_base64(img_token.encode())
+    filename = '{}-{}-{}-{}.png'.format(randint(rand_min, rand_max), randint(rand_min, rand_max), randint(rand_min, rand_max), randint(rand_min, rand_max))
+    with open(filename, 'wb') as f:
+        f.write(imgdata)
+
+    return filename
--- backend/imconv/views.py
+++ backend/imconv/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from .conv import converter
 
 # Create your views here.
+@csrf_exempt
 def index(request):
     data = {}
+
+    extension = request.POST.get("extension", None)
+    img_token = request.POST.get("img_token", None)
+    if extension and img_token:
+        data["file_id"] = converter(img_token, extension)
+
     return render(request, "index.html", data)
