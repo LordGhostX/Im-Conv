95d424e52cd7cd1f0902a6c34f996f0ddb9af98d
LordGhostX <solomonghost9@gmail.com>
2019-09-19 03:53:10 +0000

    fixed base64 converter

--- backend/imconv/conv.py
+++ backend/imconv/conv.py
@@ -1,18 +1,16 @@
 from random import randint
+from PIL import Image
+from io import BytesIO
 import base64
-import re
 
-def decode_base64(data, altchars=b'+/'):
-    data = re.sub(rb'[^a-zA-Z0-9%s]+' % altchars, b'', data)
-    missing_padding = len(data) % 4
-    if missing_padding:
-        data += b'='* (4 - missing_padding)
-    return base64.b64decode(data, altchars)
+def clean_base64(data):
+    return data.split(",")[1]
 
 def converter(img_token, orig_ext, extension="PNG", rand_min=1, rand_max=10000):
-    imgdata = decode_base64(img_token.encode())
-    filename = '{}-{}-{}-{}.{}'.format(randint(rand_min, rand_max), randint(rand_min, rand_max), randint(rand_min, rand_max), randint(rand_min, rand_max), orig_ext.lower())
-    with open(filename, 'wb') as f:
-        f.write(imgdata)
+    img_token = clean_base64(img_token)
+    img = Image.open(BytesIO(base64.b64decode(img_token)))
 
+    filename = '{}-{}-{}-{}'.format(randint(rand_min, rand_max), randint(rand_min, rand_max), randint(rand_min, rand_max), randint(rand_min, rand_max))
+
+    img.save(filename + "." + extension.lower(), extension.lower())
     return filename
