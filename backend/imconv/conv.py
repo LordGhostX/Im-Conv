d9333d88e443b4747ecd9b3721582d7ed9640fd2
LordGhostX <solomonghost9@gmail.com>
2019-09-19 05:15:24 +0000

    fixed pdf ish

--- backend/imconv/conv.py
+++ backend/imconv/conv.py
@@ -12,5 +12,12 @@ def converter(img_token, orig_ext, extension="PNG", rand_min=1, rand_max=10000):
 
     filename = '{}-{}-{}-{}'.format(randint(rand_min, rand_max), randint(rand_min, rand_max), randint(rand_min, rand_max), randint(rand_min, rand_max))
 
+    if orig_ext.lower() == "png":
+        if len(img.split()) == 4:
+            background = Image.new("RGB", img.size, (255, 255, 255))
+            background.paste(img, mask=img.split()[3])
+            img = background
+            del background
+
     img.save(filename + "." + extension.lower(), extension.lower())
     return filename
